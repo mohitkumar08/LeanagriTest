d8eb42d715f3025c8251880aa614ba3744f75dab
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-11 21:49:51 +0530

    Add movies list view model

---
+++ app/src/main/java/com/leanagritest/ui/movielist/MovieListViewModel.kt
@@ -0,0 +1,8 @@
+package com.leanagritest.ui.movielist
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MovieListViewModel (application: Application) : AndroidViewModel(application) {
+
+}
