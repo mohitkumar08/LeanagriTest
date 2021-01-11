204622cb2e1f3e6d623ed5023c64e7ff33b355f2
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-11 17:01:15 +0530

    Add launcher activity

---
+++ app/src/main/java/com/leanagritest/ui/launcher/LauncherActivity.kt
@@ -0,0 +1,12 @@
+package com.leanagritest.ui.launcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leanagritest.R
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
