dcc113425a2f0257154e27e6ae91499a9987a7be
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-11 21:45:56 +0530

    AppObjectController.kt -  all config object create in this file

---
+++ app/src/main/java/com/leanagritest/core/AppObjectController.kt
@@ -0,0 +1,80 @@
+package com.leanagritest.core
+
+
+import android.content.Context
+import android.util.Log
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.google.gson.*
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.leanagritest.BuildConfig
+import com.leanagritest.LeanAgriApplication
+import com.leanagritest.repository.local.AppDatabase
+import com.leanagritest.repository.service.MoviesNetworkService
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val READ_TIMEOUT = 30L
+private const val WRITE_TIMEOUT = 30L
+private const val CONNECTION_TIMEOUT = 30L
+private const val CALL_TIMEOUT = 60L
+
+class AppObjectController {
+
+    companion object {
+
+        @JvmStatic
+        var INSTANCE: AppObjectController =
+            AppObjectController()
+
+        @JvmStatic
+        lateinit var joshApplication: LeanAgriApplication
+            private set
+
+        @JvmStatic
+        lateinit var appDatabase: AppDatabase
+            private set
+
+        @JvmStatic
+        lateinit var retrofit: Retrofit
+            private set
+
+        @JvmStatic
+        lateinit var moviesNetworkService: MoviesNetworkService
+            private set
+
+        fun initLibrary(context: Context): AppObjectController {
+            joshApplication = context as LeanAgriApplication
+            appDatabase = AppDatabase.getDatabase(context)!!
+            Stetho.initializeWithDefaults(context)
+
+
+            val builder = OkHttpClient().newBuilder()
+                .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+                .callTimeout(CALL_TIMEOUT, TimeUnit.SECONDS)
+
+            if (BuildConfig.DEBUG) {
+                val logging =
+                    HttpLoggingInterceptor { message -> Log.e("OkHttp", message) }.apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+
+                    }
+                builder.addInterceptor(logging)
+                builder.addNetworkInterceptor(StethoInterceptor())
+            }
+            retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .client(builder.build())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create(Gson()))
+                .build()
+            moviesNetworkService = retrofit.create(MoviesNetworkService::class.java)
+            return INSTANCE
+        }
+    }
+}
