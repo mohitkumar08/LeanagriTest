04fe44c2cb2cf531a1cd5895a2be33fe1a98900c
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-14 21:50:09 +0530

    Add function for load round image

--- app/src/main/java/com/leanagritest/core/Utils.kt
+++ app/src/main/java/com/leanagritest/core/Utils.kt
@@ -1,9 +1,16 @@
 package com.leanagritest.core
 
 import android.content.Context
+import android.content.res.Resources
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 object Utils {
 
@@ -26,5 +33,22 @@ object Utils {
             return nwInfo.isConnected
         }
     }
+    private fun dpToPx(dp: Int): Int {
+        return (dp * Resources.getSystem().displayMetrics.density).toInt()
+    }
+
+
+    fun ImageView.setRoundImage(
+        url: String,
+        context: Context = AppObjectController.joshApplication
+    ) {
+        val multi = MultiTransformation(RoundedCornersTransformation(dpToPx(8), 0))
+        Glide.with(context)
+            .load(url)
+            .override(Target.SIZE_ORIGINAL)
+            .apply(RequestOptions.bitmapTransform(multi))
+            .into(this)
+    }
+
 
 }
