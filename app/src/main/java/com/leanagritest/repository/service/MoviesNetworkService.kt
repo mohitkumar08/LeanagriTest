e64a923534e24d5380d78a5f64a51eebd29bf5d2
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-16 21:32:21 +0530

    refactor api

--- app/src/main/java/com/leanagritest/repository/service/MoviesNetworkService.kt
+++ app/src/main/java/com/leanagritest/repository/service/MoviesNetworkService.kt
@@ -2,17 +2,18 @@ package com.leanagritest.repository.service
 
 import com.leanagritest.repository.server.MovieDetail
 import com.leanagritest.repository.server.ResponseMovies
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
-
-const val DIR = "3/discover/movie"
+import java.util.HashMap
 
 interface MoviesNetworkService {
 
-    @GET(DIR)
-    suspend fun getMovies(@QueryMap params: Map<String, String>): ResponseMovies
+    @GET("discover/movie")
+    suspend fun getMovies(@QueryMap params: HashMap<String, String>): ResponseMovies
 
-    @GET("$DIR/course/course_details/")
-    suspend fun getMovieDetails(@QueryMap params: Map<String, String>): MovieDetail
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(@Path("movie_id") movieId: Int, @QueryMap params: Map<String, String>): Response<MovieDetail>
 
 }
