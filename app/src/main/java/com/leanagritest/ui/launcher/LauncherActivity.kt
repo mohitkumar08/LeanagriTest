c688c18a43c5f5e31868ac5ac59b7f15cdabe37f
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-16 21:34:14 +0530

    Refactor code

--- app/src/main/java/com/leanagritest/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/com/leanagritest/ui/launcher/LauncherActivity.kt
@@ -1,12 +1,19 @@
 package com.leanagritest.ui.launcher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.leanagritest.R
+import com.leanagritest.ui.movielist.MoviesListActivity
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+        Handler(Looper.getMainLooper()).postDelayed({
+            MoviesListActivity.startMoviesListActivity(this)
+            finish()
+        }, 1500)
     }
 }
