4a67d0298584e5763b697f38d0336ebd35a62e21
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-16 21:36:03 +0530

    Add movies list in UI

--- app/src/main/java/com/leanagritest/ui/movielist/MoviesListActivity.kt
+++ app/src/main/java/com/leanagritest/ui/movielist/MoviesListActivity.kt
@@ -1,25 +1,60 @@
 package com.leanagritest.ui.movielist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.leanagritest.R
+import com.leanagritest.repository.local.entity.MovieModel
+import com.leanagritest.ui.moviedetails.MovieDetailsActivity
+import com.leanagritest.ui.movielist.adapter.MoviesListAdapter
+import com.leanagritest.ui.movielist.adapter.OnMoviesSelectListener
+import kotlinx.android.synthetic.main.activity_movies_list.*
 
-class MoviesListActivity : AppCompatActivity() {
+class MoviesListActivity : AppCompatActivity(), OnMoviesSelectListener {
+
+    companion object {
+        fun startMoviesListActivity(activity: Activity) {
+            Intent(activity, MoviesListActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }.run {
+                activity.startActivity(this)
+            }
+        }
+    }
 
     private val viewModel: MovieListViewModel by lazy {
         ViewModelProvider(this).get(MovieListViewModel::class.java)
     }
-
+    private var moviesListAdapter: MoviesListAdapter = MoviesListAdapter(listener = this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies_list)
+        initView()
+        addObserver()
+        viewModel.getMovies()
+    }
 
+    private fun initView() {
+        recycler_view.layoutManager = LinearLayoutManager(this).apply {
+            isSmoothScrollbarEnabled = true
+        }
+        recycler_view.adapter = moviesListAdapter
+    }
+
+    private fun addObserver() {
+        viewModel.listOfMoviesLiveData.observe(this, { list ->
+            list?.let {
+                moviesListAdapter.addMovies(it)
+            }
+        })
+    }
+
+    override fun onClickMovie(movie: MovieModel) {
+        MovieDetailsActivity.showDetailsOfMovie(this, movie)
     }
 }
-//https://developers.themoviedb.org/3/discover/movie-discover
-//https://developers.themoviedb.org/3/configuration/get-api-configuration
-//https://developers.themoviedb.org/3/movies/get-movie-details
-///https://developers.themoviedb.org/3/movies/get-movie-credits
-//https://developers.themoviedb.org/3/movies/get-similar-movies
--- app/src/main/java/com/leanagritest/ui/movielist/adapter/MoviesListAdapter.kt
+++ app/src/main/java/com/leanagritest/ui/movielist/adapter/MoviesListAdapter.kt
@@ -2,11 +2,17 @@ package com.leanagritest.ui.movielist.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.leanagritest.core.Utils.setRoundImage
 import com.leanagritest.databinding.MoviesListItemViewBinding
 import com.leanagritest.repository.local.entity.MovieModel
 
-class MoviesListAdapter(private val movieList: ArrayList<MovieModel> = arrayListOf()) :
+
+class MoviesListAdapter(
+    private val movieList: ArrayList<MovieModel> = arrayListOf(),
+    private val listener: OnMoviesSelectListener? = null
+) :
     RecyclerView.Adapter<MoviesListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,14 +27,33 @@ class MoviesListAdapter(private val movieList: ArrayList<MovieModel> = arrayList
 
     override fun getItemCount() = movieList.size
 
+    fun addMovies(mNewMovieList: List<MovieModel>) {
+        val diffCallback = MoviesDiffCallback(movieList, mNewMovieList)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        movieList.clear()
+        movieList.addAll(mNewMovieList)
+        diffResult.dispatchUpdatesTo(this)
+    }
 
     inner class ViewHolder(private val binding: MoviesListItemViewBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(certificationQuestion: MovieModel) {
+        fun bind(moviesModel: MovieModel) {
             with(binding) {
-
+                tvMovieName.text = moviesModel.originalTitle
+                tvRating.text = moviesModel.rating.toString()
+                tvYear.text = moviesModel.releaseDate.split("-")[0]
+                if (moviesModel.posterPath.isNullOrEmpty().not()) {
+                    imageView.setRoundImage(moviesModel.getModifiedPath())
+                }
+                root.setOnClickListener {
+                    listener?.onClickMovie(moviesModel)
+                }
             }
         }
     }
+}
+
+interface OnMoviesSelectListener {
+    fun onClickMovie(movie: MovieModel)
 }
