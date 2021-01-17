5090a1c1b42690b44c96ef5143325ac85104bf47
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-17 16:00:34 +0530

    add different type of filter

--- app/src/main/java/com/leanagritest/ui/movielist/MovieListViewModel.kt
+++ app/src/main/java/com/leanagritest/ui/movielist/MovieListViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.leanagritest.ApiCallStatus
+import com.leanagritest.SortBased
 import com.leanagritest.core.AppObjectController
 import com.leanagritest.core.Utils
 import com.leanagritest.repository.local.entity.MovieModel
@@ -15,6 +17,9 @@ import java.util.*
 class MovieListViewModel(application: Application) : AndroidViewModel(application) {
     private val _listOfMoviesMutableLiveData: MutableLiveData<List<MovieModel>> = MutableLiveData()
     val listOfMoviesLiveData: LiveData<List<MovieModel>> = _listOfMoviesMutableLiveData
+    val apiCallStatusLiveData: MutableLiveData<ApiCallStatus> = MutableLiveData()
+     val clearListLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    private var sortBased: SortBased = SortBased.DATE_MAX_TO_MIN
     private var pageNumber = 1
     private var totalPage = 1
 
@@ -23,15 +28,14 @@ class MovieListViewModel(application: Application) : AndroidViewModel(applicatio
     init {
         queryMap.apply {
             put("language", "en-US")
-      //      put("sort_by", "release_date.desc")
+            put("sort_by", "release_date.desc")
             put("page", "1")
-            put("primary_release_date.gte","2014-09-15")
-            put("primary_release_date.lte","2019-01-14")
-
+            //put("primary_release_date.gte", "2014-09-15")
+           // put("primary_release_date.lte", "2019-01-14")
         }
     }
 
-////URL: /discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22
+    ////URL: /discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22
     fun getMovies() {
         viewModelScope.launch(Dispatchers.IO) {
             if (Utils.isInternetAvailable()) {
@@ -51,8 +55,11 @@ class MovieListViewModel(application: Application) : AndroidViewModel(applicatio
                 pageNumber = responseObj.page
                 totalPage = responseObj.totalPages
                 saveInDbForOffline(responseObj.results)
+                pageNumber++
+                apiCallStatusLiveData.postValue(ApiCallStatus.SUCCESS)
             } catch (ex: Exception) {
                 ex.printStackTrace()
+                apiCallStatusLiveData.postValue(ApiCallStatus.FAILED)
             }
         }
     }
@@ -69,7 +76,76 @@ class MovieListViewModel(application: Application) : AndroidViewModel(applicatio
 
     private fun getQueryParams(): HashMap<String, String> {
         queryMap["page"] = pageNumber.toString()
-        queryMap["sort_by"] = pageNumber.toString()
         return queryMap
     }
+    private fun reset(){
+        pageNumber = 1
+        totalPage=1
+    }
+
+    fun getMoviesSortByDateHighToLow() {
+        if (SortBased.DATE_MAX_TO_MIN == sortBased) {
+            return
+        }
+        sortBased = SortBased.DATE_MAX_TO_MIN
+        clearListLiveData.postValue(true)
+        reset()
+        queryMap["sort_by"] = "release_date.desc"
+        getFromServer()
+    }
+
+    fun getMoviesSortByDateLowToHigh() {
+        if (SortBased.DATE_MIN_TO_MAX == sortBased) {
+            return
+        }
+        sortBased = SortBased.DATE_MIN_TO_MAX
+        clearListLiveData.postValue(true)
+        reset()
+        queryMap["sort_by"] = "release_date.asc"
+        getFromServer()
+    }
+
+    fun getMoviesSortByRatingLowToHigh() {
+        if (SortBased.RATING_LOW_TO__HIGH == sortBased) {
+            return
+        }
+        sortBased = SortBased.RATING_LOW_TO__HIGH
+        clearListLiveData.postValue(true)
+        reset()
+        queryMap["sort_by"] = "vote_average.asc"
+        getFromServer()
+    }
+
+    fun getMoviesSortByRatingHighToLow() {
+        if (SortBased.RATING_HIGH_TO_LOW == sortBased) {
+            return
+        }
+        sortBased = SortBased.RATING_HIGH_TO_LOW
+        clearListLiveData.postValue(true)
+        reset()
+        queryMap["sort_by"] = "vote_average.desc"
+        getFromServer()
+    }
+
+    fun getMoviesPopularityByHighToLow() {
+        if (SortBased.POPULARITY_HIGH_TO_LOW == sortBased) {
+            return
+        }
+        sortBased = SortBased.POPULARITY_HIGH_TO_LOW
+        clearListLiveData.postValue(true)
+        reset()
+        queryMap["sort_by"] = "popularity.desc"
+        getFromServer()
+    }
+    fun getMoviesPopularityByLowToHigh() {
+        if (SortBased.POPULARITY_LOW_TO_HIGH == sortBased) {
+            return
+        }
+        sortBased = SortBased.POPULARITY_LOW_TO_HIGH
+        clearListLiveData.postValue(true)
+        reset()
+        queryMap["sort_by"] = "popularity.asc"
+        getFromServer()
+    }
+
 }
