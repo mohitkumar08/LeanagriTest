d137656d71caee1b749ccc0a92b3ddc2d41acc1f
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-17 15:59:09 +0530

    Add enum for sort

--- app/src/main/java/com/leanagritest/StaticConstant.kt
+++ app/src/main/java/com/leanagritest/StaticConstant.kt
@@ -3,3 +3,11 @@ package com.leanagritest
 enum class ApiCallStatus {
      SUCCESS, FAILED, NOT_FOUND
 }
+enum class SortBased{
+     RATING_LOW_TO__HIGH,
+     RATING_HIGH_TO_LOW,
+     DATE_MIN_TO_MAX,
+     DATE_MAX_TO_MIN,
+     POPULARITY_LOW_TO_HIGH,
+     POPULARITY_HIGH_TO_LOW,
+}
